a1a650bbab3e6a62fa7609d239ebbaeb1105e351
antso.tech <hei.antso.2007@gmail.com>
2025-10-07 09:26:53 +0300

    ajout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 const titleExpense = document.getElementById('expense-title')
 const expensePrice = document.getElementById('expense-price')
 const selectCategory = document.getElementById('select-category')
+const cancelExpense = document.getElementById('cancel')
+const form = document.querySelector('form')
+
+const title = "";
+const price = 0;
+const category = ""
+console.log(titleExpense);
+
+titleExpense.addEventListener("input",(e) =>{
+    title = e.target.value
+    
+})
+
+expensePrice.addEventListener("input", (e) => {
+    price = e.target.value
+    
+})
+
+selectCategory.addEventListener("input",(e) => {
+     console.log(e.target.value);
+     
+})
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    document.getElementById('allExpense').innerHTML(`
+        
+        `)
+    console.log('text');
+    
+
+})
+
